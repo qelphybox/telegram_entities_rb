9acb4940a2a0f5d7675e1fc764755b0af281df35
Kirill Bobykin <qelphybox@gmail.com>
2025-11-22 15:52:20 +0400

    init gem

---
+++ lib/tg_entity.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "tg_entity/version"
+
+module TgEntity
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/tg_entity/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module TgEntity
+  VERSION = "0.1.0"
+end
