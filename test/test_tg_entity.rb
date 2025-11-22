36e87ce5924fbdc9ac2bc1fb95fdb506231bc081
Kirill Bobykin <qelphybox@gmail.com>
2025-11-22 19:15:00 +0400

    remove redundant test

--- test/test_tg_entity.rb
+++ test/test_tg_entity.rb
@@ -6,8 +6,4 @@ class TestTgEntity < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::TgEntity::VERSION
   end
-
-  def test_it_does_something_useful
-    assert false
-  end
 end
